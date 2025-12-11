1d88f017448728f4660e5cd9253519aa3c5918ea
Jon Crump <s3022659@live.tees.ac.uk>
2025-12-11 14:59:36 +0000

    auth0

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import './assets/main.css';
+
 import { createApp } from 'vue';
-import { createAuth0 } from '@auth0/auth0-vue';
-import { appConfig, buildAuth0Options } from './config/appConfig';
-import App from './App.vue';
+import { createPinia } from 'pinia';
 import router from './router';
+import { createAuth0 } from '@auth0/auth0-vue';
+
+import App from './App.vue';
+import { appConfig, buildAuth0Options } from './config/appConfig';
 
 const app = createApp(App);
 
@@ -11,6 +15,7 @@ app.use(createAuth0(buildAuth0Options(appConfig)));
 // Optionally provide the config to the app for injection where needed
 app.provide('appConfig', appConfig);
 
+app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
